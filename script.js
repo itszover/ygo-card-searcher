3c7505d762493654eb6571058e400883aa2ed04d
José Augusto Gomes Martinho <jose.martinho@outlook.com.br>
2024-03-19 20:00:52 +0000

    criando e modificando fns

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const span = document.querySelector('span');
 const input = document.querySelector('#card_input');
+const small = document.querySelector('small');
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
     if (isInputEmpty(input)) {
-        return console.log('vazio')
-    } 
-
-    fetchCard(input);
+        writeInnerText(small, 'Campo vazio.');
+    } else {
+        writeInnerText(small, '');
+        fetchCard(input);
+    }
 });
 
 async function fetchCard(input) {
@@ -15,18 +17,26 @@ async function fetchCard(input) {
         const response = await fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${input.value}`);
 
         if (!response.ok) {
+            writeInnerText(small, 'Carta não encontrada.');
             throw new Error('Carta não encontrada');
         }
 
         const data = await response.json();
         const cardName = data.data[0].name;
-        span.innerText = cardName;
+        writeInnerText(span, cardName);
     } catch (error) {
         console.error(`Erro: ${error}`);
     }
 }
 
 function isInputEmpty(input) {
-    if (input.value.trim() === '') return true;
-    else return false;
+    if (input.value.trim() === '') {
+        return true
+    }
+
+    return false;  
+}
+
+function writeInnerText(element, message) {
+    element.innerText = message;
 }
